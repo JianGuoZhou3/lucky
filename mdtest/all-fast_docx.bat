b9f78462e5073928a01f7c186165cbc8f5ef7c57
huangwb8 <654751191@qq.com>
2019-09-05 14:36:48 +0800

    daily update

--- mdtest/all-fast_docx.bat
+++
@@ -1,3 +0,0 @@
-pandoc --filter pandoc-citeproc --bibliography=E:/RCloud/RFactory/lucky/mdtest/exosome.bib --csl=E:/RCloud/RFactory/lucky/mdtest/clinical-cancer-research.csl E:/RCloud/RFactory/lucky/mdtest/hnRNPA2B1_1.md -o E:/RCloud/RFactory/lucky/mdtest/hnRNPA2B1_1.docx
-pandoc --filter pandoc-citeproc --bibliography=E:/RCloud/RFactory/lucky/mdtest/exosome.bib --csl=E:/RCloud/RFactory/lucky/mdtest/clinical-cancer-research.csl E:/RCloud/RFactory/lucky/mdtest/hnRNPA2B1_2.md -o E:/RCloud/RFactory/lucky/mdtest/hnRNPA2B1_2.docx
-pandoc --filter pandoc-citeproc --bibliography=E:/RCloud/RFactory/lucky/mdtest/exosome.bib --csl=E:/RCloud/RFactory/lucky/mdtest/clinical-cancer-research.csl E:/RCloud/RFactory/lucky/mdtest/hnRNPA2B1_3.md -o E:/RCloud/RFactory/lucky/mdtest/hnRNPA2B1_3.docx
